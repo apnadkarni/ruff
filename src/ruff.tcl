cd67b9ae4c3a484e0eb43cc6909556dfe61589c0
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2026-01-13 16:36:15 +0530

    Bump to 2.8.0

--- src/ruff.tcl
+++ src/ruff.tcl
@@ -19,7 +19,7 @@ msgcat::mcload [file join [file dirname [info script]] msgs]
 
 namespace eval ruff {
     # If you change version here, change in pkgIndex.tcl as well
-    variable version 2.7.0
+    variable version 2.8.0
     proc version {} {
         # Returns the Ruff! version.
         variable version
